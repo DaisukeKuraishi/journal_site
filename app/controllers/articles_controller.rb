75f16a2470aa5e69aaed9e208d2e9a11983c41de
DaisukeKuraishi <madgga@gmail.com>
2018-03-12 13:01:27 +0900

    記事一覧と詳細、一覧へ戻るを作成

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,15 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def view
+  end
+
+  def new
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'articles/index'
+
+  get 'articles/view'
+
+  get 'articles/new'
+
+  get 'articles/show'
+
+  resources :articles
 end
