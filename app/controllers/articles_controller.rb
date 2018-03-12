163f594a45e72ef54f87e2c8cd483b7de1bf1dfc
DaisukeKuraishi <madgga@gmail.com>
2018-03-12 14:42:45 +0900

    課題論文投稿サイト終了

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,13 +3,26 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def view
-  end
-
-  def new
-  end
-
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    # 上の行はインスタンスを作っただけ。saveメソッドで保存しないと、消える。
+    @article.save
+    # 処理の後はindex画面を表示
+    redirect_to articles_path
+  end
+
+
+  private
+  def article_params
+      params.require(:article).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-
-  get 'articles/view'
-
-  get 'articles/new'
-
-  get 'articles/show'
 
   resources :articles
+
+# 　root 'articles#index'
+
 end
